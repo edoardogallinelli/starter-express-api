5b3a45014651ad8bf63b3901e03b06841f3c8a13
edoardogallinelli <37400390+edoardogallinelli@users.noreply.github.com>
2024-02-17 16:01:00 +0100

    +

--- mail-service.js
+++ mail-service.js
@@ -2,40 +2,41 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 async function serviceMail(data) {
 
-  console.log('chiamato service send mail');
-
-
-
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.MAIL,
-      pass: process.env.APP_PSW,
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.MAIL,
-    to: "gallinelliroma@gmail.com",
-    cc: ['edoardogallinelli@gmail.com', 'edoardogallinelli.job@gmail.com'],
-    subject: "Richiesta di acquisto Dominio",
-    text: `Ecco i dettagli: 
-          nome: ${data.recapiti.name} ,
-          cognome: ${data.recapiti.lastName} ,
-          Telefono: ${data.recapiti.phone} ,
-          Mail: ${data.recapiti.mail} ,
-          Testo: ${data.recapiti.text},
-          Domini selezionati: ${data.dominiRichiesti}
-
-          N.B.: ricordati di rispondere il prima prossibile.
-   `
-  };
-  console.log(mailOptions, 'options')
 
   await new Promise((resolve, reject) => {
+    console.log('chiamato service send mail');
+
+
+
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.MAIL,
+        pass: process.env.APP_PSW,
+      },
+    });
+  
+    const mailOptions = {
+      from: process.env.MAIL,
+      to: "gallinelliroma@gmail.com",
+      cc: ['edoardogallinelli@gmail.com', 'edoardogallinelli.job@gmail.com'],
+      subject: "Richiesta di acquisto Dominio",
+      text: `Ecco i dettagli: 
+            nome: ${data.recapiti.name} ,
+            cognome: ${data.recapiti.lastName} ,
+            Telefono: ${data.recapiti.phone} ,
+            Mail: ${data.recapiti.mail} ,
+            Testo: ${data.recapiti.text},
+            Domini selezionati: ${data.dominiRichiesti}
+  
+            N.B.: ricordati di rispondere il prima prossibile.
+     `
+    };
+    console.log(mailOptions, 'options')
+  
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
         console.log("error is " + error);
@@ -47,7 +48,7 @@ async function serviceMail(data) {
       }
     });
 
-    transporter.verify().then(console.log).catch(console.error);
+    // transporter.verify().then(console.log).catch(console.error);
   })
 
 }
