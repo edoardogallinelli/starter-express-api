c9068464c98a019412dcf9f04ee1e9808b8946ff
edoardogallinelli <37400390+edoardogallinelli@users.noreply.github.com>
2024-02-17 14:34:55 +0100

    wrapped into promise

--- mail-service.js
+++ mail-service.js
@@ -4,23 +4,72 @@ function serviceMail(data) {
 
   console.log('chiamato service send mail');
 
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.MAIL,
-      pass: process.env.APP_PSW,
-    },
-  });
+  // const transporter = nodemailer.createTransport({
+  //   service: "Gmail",
+  //   host: "smtp.gmail.com",
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     user: process.env.MAIL,
+  //     pass: process.env.APP_PSW,
+  //   },
+  // });
 
-  const mailOptions = {
-    from: process.env.MAIL,
-    to: "gallinelliroma@gmail.com",
-    cc: ['edoardogallinelli@gmail.com', 'edoardogallinelli.job@gmail.com'],
-    subject: "Richiesta di acquisto Dominio",
-    text: `Ecco i dettagli: 
+  // const mailOptions = {
+  //   from: process.env.MAIL,
+  //   to: "gallinelliroma@gmail.com",
+  //   cc: ['edoardogallinelli@gmail.com', 'edoardogallinelli.job@gmail.com'],
+  //   subject: "Richiesta di acquisto Dominio",
+  //   text: `Ecco i dettagli: 
+  //         nome: ${data.recapiti.name} ,
+  //         cognome: ${data.recapiti.lastName} ,
+  //         Telefono: ${data.recapiti.phone} ,
+  //         Mail: ${data.recapiti.mail} ,
+  //         Testo: ${data.recapiti.text},
+  //         Domini selezionati: ${data.dominiRichiesti}
+
+  //         N.B.: ricordati di rispondere il prima prossibile.
+  //  `
+  // };
+  // console.log(mailOptions, 'options')
+  // // transporter.sendMail(mailOptions, (error, info) => {
+  // //   if (error) {
+  // //     console.error("Error sending email: ", error);
+  // //   } else {
+
+  // //   }
+  // // });
+
+  // // new Promise((resolve, reject) => {
+  // //   transporter.sendMail(mailOptions, function (error, info) {
+  // //     if (error) {
+  // //       reject(error)
+  // //     } else {
+  // //       console.log("Email sent: ", info.response);
+  // //     }
+  // //   });
+  // // })
+
+
+  return new Promise((resolve, reject) => {
+
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.MAIL,
+        pass: process.env.APP_PSW,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.MAIL,
+      to: "gallinelliroma@gmail.com",
+      cc: ['edoardogallinelli@gmail.com', 'edoardogallinelli.job@gmail.com'],
+      subject: "Richiesta di acquisto Dominio",
+      text: `Ecco i dettagli: 
           nome: ${data.recapiti.name} ,
           cognome: ${data.recapiti.lastName} ,
           Telefono: ${data.recapiti.phone} ,
@@ -30,26 +79,24 @@ function serviceMail(data) {
 
           N.B.: ricordati di rispondere il prima prossibile.
    `
-  };
-  console.log(mailOptions, 'options')
-  // transporter.sendMail(mailOptions, (error, info) => {
-  //   if (error) {
-  //     console.error("Error sending email: ", error);
-  //   } else {
-      
-  //   }
-  // });
+    };
+    console.log(mailOptions, 'options')
+    let resp = false;
 
-  new Promise((resolve, reject) => {
     transporter.sendMail(mailOptions, function (error, info) {
-          if (error) {
-              reject(error)
-          } else {
-            console.log("Email sent: ", info.response);
-          }
-      });
-})
-  transporter.verify().then(console.log).catch(console.error);
+      if (error) {
+        console.log("error is " + error);
+        resolve(false); // or use rejcet(false) but then you will have to handle errors
+      }
+      else {
+        console.log('Email sent: ' + info.response);
+        resolve(true);
+      }
+    });
+
+    transporter.verify().then(console.log).catch(console.error);
+  })
+
 }
 module.exports = serviceMail;
 
