40a57a40945eb1a541afec8805073412b4641f60
edoardogallinelli <37400390+edoardogallinelli@users.noreply.github.com>
2024-02-17 13:28:30 +0100

    add logs

--- mail-service.js
+++ mail-service.js
@@ -1,40 +1,8 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 function serviceMail(data) {
-  // console.log("chiamato");
-  // const transporter = nodemailer.createTransport({
-  //   host: process.env.SMTP_HOST || 'smtp-relay.brevo.com',
-  //   port: process.env.SMTP_PORT || 587,
-  //   auth: {
-  //     user: process.env.USER,
-  //     pass: process.env.KEY,
-  //   },
-  //   debug: false,
-  //   logger: true,
-  // });
-  // console.log(data);
-  // const info = transporter.sendMail({
-  //   from: `edoardogallinelli.job@gmail.com`, // sender address
-  //   to: "edoardogallinelli.job@gmail.com", // list of receivers
-  //   subject: "Occhio pà che vogliono comprare qualche dominio ", // Subject line
-  //   text: `Ecco i dettagli: 
-  //   nome: ${data.recapiti.name} ,
-  //   cognome: ${data.recapiti.lastName} ,
-  //   Telefono: ${data.recapiti.phone} ,
-  //   Mail: ${data.recapiti.mail} ,
-  //   Testo: ${data.recapiti.text},
-  //   Domini selezionati: ${data.dominiRichiesti}
-  //   `, // plain text body
-  // });
 
-  // transporter.sendMail(info, (error, info) => {
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //     console.log("Email sent: " + info.response);
-  //   }
-  // });
-  // transporter.verify().then(console.log).catch(console.error);
+  console.log('chiamato service send mail');
 
   const transporter = nodemailer.createTransport({
     service: "Gmail",
@@ -49,7 +17,8 @@ function serviceMail(data) {
 
   const mailOptions = {
     from: process.env.MAIL,
-    to: "edoardogallinelli.job@gmail.com",
+    to: "gallinelliroma@gmail.com",
+    cc: ['edoardogallinelli@gmail.com', 'edoardogallinelli.job@gmail.com'],
     subject: "Richiesta di acquisto Dominio",
     text: `Ecco i dettagli: 
           nome: ${data.recapiti.name} ,
@@ -62,7 +31,7 @@ function serviceMail(data) {
           N.B.: ricordati di rispondere il prima prossibile.
    `
   };
-
+  console.log(mailOptions, 'options')
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error("Error sending email: ", error);
@@ -70,6 +39,8 @@ function serviceMail(data) {
       console.log("Email sent: ", info.response);
     }
   });
+
+  transporter.verify().then(console.log).catch(console.error);
 }
 module.exports = serviceMail;
 
