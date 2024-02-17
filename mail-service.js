09f9760b9339681c3fe03425779fbbf4434321f0
edoardogallinelli <37400390+edoardogallinelli@users.noreply.github.com>
2024-02-17 15:47:18 +0100

    minor change

--- mail-service.js
+++ mail-service.js
@@ -4,60 +4,13 @@ function serviceMail(data) {
 
   console.log('chiamato service send mail');
 
-  // const transporter = nodemailer.createTransport({
-  //   service: "Gmail",
-  //   host: "smtp.gmail.com",
-  //   port: 465,
-  //   secure: true,
-  //   auth: {
-  //     user: process.env.MAIL,
-  //     pass: process.env.APP_PSW,
-  //   },
-  // });
-
-  // const mailOptions = {
-  //   from: process.env.MAIL,
-  //   to: "gallinelliroma@gmail.com",
-  //   cc: ['edoardogallinelli@gmail.com', 'edoardogallinelli.job@gmail.com'],
-  //   subject: "Richiesta di acquisto Dominio",
-  //   text: `Ecco i dettagli: 
-  //         nome: ${data.recapiti.name} ,
-  //         cognome: ${data.recapiti.lastName} ,
-  //         Telefono: ${data.recapiti.phone} ,
-  //         Mail: ${data.recapiti.mail} ,
-  //         Testo: ${data.recapiti.text},
-  //         Domini selezionati: ${data.dominiRichiesti}
-
-  //         N.B.: ricordati di rispondere il prima prossibile.
-  //  `
-  // };
-  // console.log(mailOptions, 'options')
-  // // transporter.sendMail(mailOptions, (error, info) => {
-  // //   if (error) {
-  // //     console.error("Error sending email: ", error);
-  // //   } else {
-
-  // //   }
-  // // });
-
-  // // new Promise((resolve, reject) => {
-  // //   transporter.sendMail(mailOptions, function (error, info) {
-  // //     if (error) {
-  // //       reject(error)
-  // //     } else {
-  // //       console.log("Email sent: ", info.response);
-  // //     }
-  // //   });
-  // // })
-
-
   return new Promise((resolve, reject) => {
 
     const transporter = nodemailer.createTransport({
       service: "Gmail",
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.MAIL,
         pass: process.env.APP_PSW,
